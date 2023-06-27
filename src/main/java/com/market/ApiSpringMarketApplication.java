67217899d3ee4e2756b82c18261575aa10db0c60
DESKTOP-HLHOE4T\juanp <juanpablotorresp.17@gmail.com>
2023-06-26 21:19:56 -0500

    First commit

---
+++ src/main/java/com/market/ApiSpringMarketApplication.java
@@ -0,0 +1,13 @@
+package com.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringMarketApplication.class, args);
+	}
+
+}
